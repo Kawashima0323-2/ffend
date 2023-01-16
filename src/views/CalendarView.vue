77e908ed96006c50902a710b925e2d21df211d12
Kawashima0323-2 <hiroki.k.0323@icloud.com>
2023-01-16 09:45:56 +0900

    1/16

---
+++ src/views/CalendarView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  <Header />
+  <Calendar />
+  <Footer />
+  </div>
+</template>
+<script>
+import Calendar from '../components/Calendar.vue'
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
+//import Navigation from '../components/Navigation.vue'
+export default {
+   
+      components: {
+        Calendar,
+        Header,
+        Footer,
+       
+        //Navigation
+  }
+}
+</script>
